1ede17dde76287b196ad1907f22682bcf51d90d5
Pro100sasha_HD <sasha_ablin@rambler.ru>
2024-11-28 21:33:55 +0500

    task2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -2,22 +2,26 @@ package com.example.task02;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.nio.file.Files;
+import java.util.stream.Stream;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-
-        /*
+        
         System.out.println(listFiles(Paths.get("task02/src/main/resources/")));
-        */
 
     }
 
     public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
-        // your implementation here
-
-        return null;
+        List<Path> list = new ArrayList<>();
+        try (Stream<Path> paths = Files.walk(rootDir)) {
+            paths.filter(Files::isRegularFile).forEach(list::add);
+        }
+        return list;
     }
 }
